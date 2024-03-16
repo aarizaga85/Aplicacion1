7fcd5fc32abd6ec150adf2f7a3d902941675fcba
aarizaga85 <andresarizaga@gmail.com>
2024-03-16 13:59:05 -0500

    Revert "Aprender Column, Row y Box"

--- app/src/main/java/com/example/aplicacion1/MainActivity.kt
+++ app/src/main/java/com/example/aplicacion1/MainActivity.kt
@@ -3,24 +3,13 @@ package com.example.aplicacion1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.aplicacion1.ui.theme.Aplicacion1Theme
 
 class MainActivity : ComponentActivity() {
@@ -38,67 +27,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-//    Box( modifier = Modifier
-//            .size(400.dp)) {
-//        Text(
-//            text = "Text1",
-//            color = Color.Blue,
-//            fontSize = 20.sp,
-//            modifier = Modifier
-//                .align(Alignment.BottomStart)
-//        )
-//        Text(
-//            text = "Text2",
-//            color = Color.Blue,
-//            fontSize = 20.sp,
-//            modifier = Modifier
-//                .align(Alignment.TopEnd)
-//        )
-//    }
-    Column(
-        modifier = Modifier
-            .background(Color.Red)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row {
-            Text(
-                text = "Text1",
-                color = Color.Blue,
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .size(80.dp)
-            )
-            Text(
-                text = "Text2",
-                color = Color.Blue,
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .size(80.dp)
-            )
-        }
-        Text(
-            text = "Hola $name!",
-            color = Color.Blue,
-            fontSize = 40.sp
-        )
-        Text(
-            text = "Otro texto",
-            color = Color.Blue,
-            fontSize = 40.sp
-        )
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
 }
 
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Aplicacion1Theme {
-        Greeting("Andres")
+        Greeting("Android")
     }
 }
